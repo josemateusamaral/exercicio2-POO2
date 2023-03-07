a36c089e8407590a5c28e51a2172e5d18704490d
José Mateus Amaral de Miranda <50233573+josemateusamaral@users.noreply.github.com>
2023-03-07 20:49:40 -0300

    Update classeComposicao.py

--- classeComposicao.py
+++ classeComposicao.py
@@ -4,9 +4,6 @@ class ChatBOT():
     def __init__(self,nome,idade,altura,peso):
         self.simulacaoDeUmaPessoa = Pessoa(nome,idade,altura,peso)
 
-    def fazerCoco(self):
-        self.simulacaoDeUmaPessoa.fazerCoco()
-
     def seApresentar(self):
         self.simulacaoDeUmaPessoa.seApresentar()
 
